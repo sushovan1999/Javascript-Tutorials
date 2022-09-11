d4c141027a596c0f35aaadfeff18e720653641b8
Sushovan Roy <33160274+sushovan1999@users.noreply.github.com>
2022-09-11 16:48:22 +0530

    10-09-22

---
+++ Javascript/js/test.js
@@ -0,0 +1,41 @@
+// let sum = 0;
+// for (let i = 0, j = 0; i < 5 & j < 5; ++i, j = i + 1)
+// sum += i;
+// console.log(sum);
+// let x = 2;
+// let y = 0;
+// for ( ; y < 10; ++y)
+// {
+// if (y % x == 0)
+// continue;
+// else if (y == 8)
+// break;
+// else
+// console.log(y + " ");
+// }
+// const a=10,b=20;
+// while(a<b)
+// {
+// console.log("Hello");
+// }
+// console.log("World");
+// let a = 5;
+// let b = 10;
+// first:
+// {
+// second:
+// {
+// third:
+// {
+// if (a == b >> 1)
+// break second;
+// }
+// console.log(a);
+// }
+// console.log(b);
+// }
+// let var1 = 5;
+// let var2 = 6;
+// let var3;
+// var3 = ++ var2 * var1 / var2 + var2;
+// console.log(var3);
