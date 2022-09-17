6762c109c8e6cfbd6504acec560b7f25582d5ffd
Sushovan Roy <33160274+sushovan1999@users.noreply.github.com>
2022-09-18 00:09:49 +0530

    updated

--- Javascript/js/test.js
+++ Javascript/js/test.js
@@ -39,3 +39,20 @@
 // let var3;
 // var3 = ++ var2 * var1 / var2 + var2;
 // console.log(var3);
+function countPositivesSumNegatives(input) {
+    if (input === null || input.length < 1) {
+        return [];
+    }
+    var array = [];
+
+    for(var i = 0; i < input.length; i++) {
+        if(input[i] <= 0) {
+            array[1] += input[i];
+      } else {
+            array[0] += 1;
+      }
+    }
+    return array;
+}
+let input =[1,2,3,5,6,-1,-2,-3,-4]
+console.log(countPositivesSumNegatives(input));
